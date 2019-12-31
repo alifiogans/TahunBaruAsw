f09195fb84b139e36a31ced9e0f249f7663d2ee2
alifiogans <58583263+alifiogans@users.noreply.github.com>
2020-01-01 02:12:32 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+pkg install update && pkg install upgrade
+pkg install curl
+pkg install bash
+pkg install figlet
+pkg install gem
+gem install lolcat
+pkg install git
+git clone https://github.com/alifiogans/AutoMetasploit
+git clone https://github.com/muhammadfathul/VIRTEX
+gem install lolcat
+pip2 install mechanize
+Chmod +x *
+sh FuckNewYear.sh
